c6bd17527c654aef7548aa02a023c3df23f7733a
ronanmas <ronanmas25@gmail.com>
2025-04-04 15:14:47 +0000

    Add stuff

--- student_cfg_practice.py
+++ student_cfg_practice.py
@@ -1,8 +1,81 @@
 from bs4 import BeautifulSoup as soup
 import requests
 
-def getcoords(city, state):
+# For Inputs
+states = {
+    "al": "Alabama",
+    "ak": "Alaska",
+    "az": "Arizona",
+    "ar": "Arkansas",
+    "ca": "California",
+    "co": "Colorado",
+    "ct": "Connecticut",
+    "de": "Delaware",
+    "dc": "District of Columbia",
+    "fl": "Florida",
+    "ga": "Georgia",
+    "gu": "Guam",
+    "hi": "Hawaii",
+    "id": "Idaho",
+    "il": "Illinois",
+    "in": "Indiana",
+    "ia": "Iowa",
+    "ks": "Kansas",
+    "ky": "Kentucky",
+    "la": "Louisiana",
+    "me": "Maine",
+    "md": "Maryland",
+    "ma": "Massachusetts",
+    "mi": "Michigan",
+    "mn": "Minnesota",
+    "ms": "Mississippi",
+    "mo": "Missouri",
+    "mt": "Montana",
+    "ne": "Nebraska",
+    "nv": "Nevada",
+    "nh": "New Hampshire",
+    "nj": "New Jersey",
+    "nm": "New Mexico",
+    "ny": "New York",
+    "nc": "North Carolina",
+    "nd": "North Dakota",
+    "mp": "Northern Mariana Islands",
+    "oh": "Ohio",
+    "ok": "Oklahoma",
+    "or": "Oregon",
+    "pa": "Pennsylvania",
+    "pr": "Puerto Rico",
+    "ri": "Rhode Island",
+    "sc": "South Carolina",
+    "sd": "South Dakota",
+    "tn": "Tennessee",
+    "tx": "Texas",
+    "tt": "Trust Territories",
+    "ut": "Utah",
+    "vt": "Vermont",
+    "va": "Virginia",
+    "vi": "Virgin Islands",
+    "wa": "Washington",
+    "wv": "West Virginia",
+    "wi": "Wisconsin",
+    "wy": "Wyoming"
+}
+def state_abbrev(input):
+    for key,value in states.items():
+        if value.lower() == input.lower():
+            return key
+    return None
 
+def city_fix(input):
+    if " " in input:
+            words = input.split()
+            city = "-".join(words)
+            return city.lower()
+    else:
+        return input.lower()
+    return None
+
+def get_coords(city, state):
     url = f'https://www.wunderground.com/weather/us/{state}/{city.lower()}'
 
     req = requests.get(url)
@@ -29,88 +102,79 @@ def getcoords(city, state):
     # print(lat)
     # print(long)
 
-    coords = {"lat":lat, "long":long}
-    return coords
+   
+    return lat, long
 
-
-
-def condition_simple(city,state,type,class_):
+# Using Webscraping
+def condition_simple(city,state,type,class_,dict_name):
     url = f'https://www.wunderground.com/weather/us/{state}/{city.lower()}'
     req = requests.get(url)
     web_page = req.content
     web = soup(web_page,'html.parser')
     name_box = web.find(type, class_)
     name = name_box.text.split() 
-    dict = {"high": name}
+    
+    if dict_name == 'time':
+        dict = {dict_name: name[1:8]}
+    else: 
+        dict = {dict_name: name}
     return dict
 
 def info(condition, city, state):
+    
     if condition == "coordinates":
-        condition = getcoords(city,state,)
+        condition = get_coords(city,state)
     elif condition == "time":
-        condition = condition_simple(city,state,'p','timestamp')
+        condition = condition_simple(city,state,'p','timestamp','time')
     elif condition == "high":
-        condition = condition_simple(city,state,'span','hi')
+        condition = condition_simple(city,state,'span','hi','high')
     elif condition == "low":
-        condition = condition_simple(city,state,'span','lo')
+        condition = condition_simple(city,state,'span','lo','low')
     elif condition == "temperature":
         next
-        # condition = condition_simple('portland','me','span','test-true wu-unit wu-unit-temperature is-degree-visible ng-star-inserted')
+        # condition = condition_simple(city,state,'div','cur-temp has-degs temp40 funits','temperature')
     elif condition == "feels like":
-        condition = condition_simple(city,state,'span','temp')
+        condition = condition_simple(city,state,'span','temp','feels like')
     elif condition == "condition":
-        condition = condition_simple(city,state,'div','condition-icon small-6 medium-12 columns')
+        condition = condition_simple(city,state,'div','condition-icon small-6 medium-12 columns','condition')
     elif condition == "humidity":
-        condition = condition_simple(city,state,'span','test-false wu-unit wu-unit-humidity ng-star-inserted')
+        condition = condition_simple(city,state,'span','test-false wu-unit wu-unit-humidity ng-star-inserted','humidity')
     elif condition == "gusts":
-        condition = condition_simple(city,state,'span','test-false wu-unit wu-unit-speed ng-star-inserted')
+        condition = condition_simple(city,state,'span','test-false wu-unit wu-unit-speed ng-star-inserted','wind gusts')
     elif condition == "wind speed":
-        condition = condition_simple(city,state,'header','wind-speed')
+        condition = condition_simple(city,state,'header','wind-speed','wind speed')
     return condition
 
-
-coords = []
-time = []
-high = []
-low = []
-temp = []
-condition = []
-feels_like = []
-humidity = []
-gusts = []
-wind_speed = []
+def complete(city,state):
+    city_1 = city_fix(city)
+    state_1 = state_abbrev(state)
 
 
-coords = info("coordinates", 'portland', 'me')
-print(coords)
+    dict = {}
+    coords = []
+    time = []
+    high = []
+    low = []
+    temp = []
+    condition = []
+    feels_like = []
+    humidity = []
+    gusts = []
+    wind_speed = []
 
-time = info('time', 'portland', 'me')
-print(time)
-
-
-high = info('high', 'portland', 'me')
-print(high)
-
-low = info('low', 'portland', 'me')
-print(low)
-
-temp = info('temperature', 'portland', 'me')
-print(temp)
-
-condition = info('condition', 'portland', 'me')
-print(condition)
-
-feels_like = info('feels like', 'portland', 'me')
-print(feels_like)
-
-humidity = info('humidity', 'portland', 'me')
-print(humidity)
-
-gusts = info('gusts', 'portland', 'me')
-print(gusts)
-
-wind_speed = info('wind speed', 'portland', 'me')
-print(wind_speed)
+    coords = info("coordinates", city_1, state_1)
+    time = info('time', city_1, state_1)
+    high = info('high', city_1, state_1)
+    low = info('low', city_1, state_1)
+    temp = info('temperature', city_1, state_1)
+    condition = info('condition', city_1, state_1)
+    feels_like = info('feels like', city_1, state_1)
+    humidity = info('humidity', city_1, state_1)
+    gusts = info('gusts', city_1, state_1)
+    wind_speed = info('wind speed', city_1, state_1)
+    
+    dict = [coords, time, high, low, temp, condition, feels_like, humidity, gusts, wind_speed]
+    return dict
 
 
 
@@ -119,63 +183,31 @@ print(wind_speed)
 
 
 
+# API
+
+import requests
+import json
 
 
 
 
-# coords = getcoords('portland', 'me')
-# time = condition_simple('portland','me','p','timestamp', time)
-# high = condition_simple('portland','me','span','hi', high)
-# low = condition_simple('portland','me','span','lo', low)
-# temp = condition_simple('portland','me','span','test-true wu-unit wu-unit-temperature is-degree-visible ng-star-inserted', temp)
-# feels_like = condition_simple('portland','me','span','temp', feels_like)
-# condition = condition_simple('portland','me','div','condition-icon small-6 medium-12 columns', condition)
-# humidity = condition_simple('portland','me','span','test-false wu-unit wu-unit-humidity ng-star-inserted', humidity)
-# gusts = condition_simple('portland','me','span','test-false wu-unit wu-unit-speed ng-star-inserted', gusts)
-# wind_speed = condition_simple('portland','me','header','wind-speed', wind_speed)
+my_key = "cb89c37822875b5b47319c98f6cb1522"
 
-# print(coords)
-# print(time[1:8]) 
-# print(high)
-# print(low)
-# # print(temp) 
-# print(feels_like)
-# print(condition)
-# print(humidity[0])
-# print(wind_speed)
-# print(gusts[0])
+# Function to get live stock data for a symbol
+def get_weather(lat, lon, api_key):
+    url = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}"
+    response = requests.get(url)
+    data = response.json()
+    return data
+    # if response.status_code == 200:
+    #     data = response.json()
+    #     return data
+    # else:
+    #     return None
 
 
-# def condition_multi(url,type, class_,name):
-#     req = requests.get(url)
-#     web_page = req.content
-#     web = soup(web_page,'html.parser')
-#     name = web.find_all(type, class_)
-#     return name
+coords = get_coords("portland","me")
+weather = get_weather(coords[0],coords[1],my_key)
 
-# temp = condition_multi('https://www.wunderground.com/weather/us/me/portland','span','test-true wu-unit wu-unit-temperature is-degree-visible ng-star-inserted', temp)
-# print(temp[8])
-
-
-
-
-
-
-
-
-
-# Next Funciton
-# for a certain place in ("","") order, return the desired outputs:
-        # get_current_wx(portland, me) -> {'time': ' 8:46 AM EDT on May 3, 2021', 
-        # 'hi': '55°', 
-        # 'lo': '44°', 
-        # 'temp': '52', 
-        # 'feels': '52°', 
-        # 'condition': 'Mostly Cloudy', 
-        # 'humidity': '52', 
-        # 'wind_dir': 'NE', 
-        # 'wind_spd': '8', 
-        # 'gusts': '12 mph'}
-
-
+print(weather)
 
